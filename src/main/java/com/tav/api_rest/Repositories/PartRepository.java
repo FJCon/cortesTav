eb4cee59a4b0e855174c354676b0b6da36c5c17c
FJCon <conilfernando@gmail.com>
2024-07-21 14:41:10 -0300

    nest id incluido en Part y url para listar partes por nestId

--- src/main/java/com/tav/api_rest/Repositories/PartRepository.java
+++ src/main/java/com/tav/api_rest/Repositories/PartRepository.java
@@ -1,9 +1,17 @@
 package com.tav.api_rest.Repositories;
 
+import com.tav.api_rest.Model.Nest;
 import com.tav.api_rest.Model.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PartRepository extends JpaRepository<Part, Integer> {
+    @Query("select p from Part p where p.nest.id = :nest")
+    List<Part> findByNstId(@Param("nest") Integer nstId);
+
 }

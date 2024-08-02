0d8ec200cf7d8935a1fe07ac7069d7709661a934
unknown <conilfernando@gmail.com>
2024-08-02 14:14:55 -0300

    tabla de piezas con filtrado

--- frontend/src/components/PartList/PartList.jsx
+++ frontend/src/components/PartList/PartList.jsx
@@ -4,44 +4,70 @@ import './PartList.css'
 import Spinner from '../Spinner/Spinner'
 
 const PartList = () => {
-    const [parts, setParts] = useState([]);
-    const [isLoading, setIsLoading] = useState(true)
+  const [parts, setParts] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const consulta = async ()=>{
-        const data = await fetch(`${url.base}/parts`);
-        const response = await data.json();
-        setParts(response);
-        setIsLoading(false);
-      }
+  const consulta = async ()=>{
+      const data = await fetch(`${url.base}/parts`);
+      const response = await data.json();
+      setParts(response);
+      setIsLoading(false);
+    }
+  useEffect(()=>{
+      consulta();
+      console.log(parts);
+  },[])
+  
+  const handleChange = (e)=>{
+    const input = e.target.value;
+    const filtrado = parts.filter(part => part.prdRef.toLowerCase().includes(input.toLowerCase()))
+    setFiltered(filtrado)
+  }
 
-    useEffect(()=>{
-        consulta();
-        console.log(parts);
-    },[])
 
 
   return (
     isLoading ? <Spinner/> :
-    <table>
-        <thead>
-            <tr>
-                <th>PIEZA</th>
-                <th>ESPESOR</th>
-                <th>CANTIDAD</th>
-                <th>ACCIONES</th>
-            </tr>
-        </thead>
-        <tbody>
-           {parts.map( part =>{
-            return(
-            <tr key={part.id} id={part.id}>
-                <td className="left">{part.prdRef}</td>
-                <td>{part.nstThickness}</td>
-                <td>{part.quantity}</td>
-            </tr>)
-           })}
-        </tbody>
-    </table>
+    <div className='main-parts-container'>
+      <div className='filter-container'>
+        <p></p>
+        <input type="text" placeholder='Buscar piezas' onChange={handleChange}/>
+      </div>
+      <div className='table-container'>
+        <table className='parts-table'>
+            <thead>
+                <tr>
+                    <th>PIEZA</th>
+                    <th>ESPESOR</th>
+                    <th>CANTIDAD</th>
+                    <th>ACCIONES</th>
+                </tr>
+            </thead>
+            <tbody>
+              {filtered.length == 0 ? 
+               parts.map( part =>{
+                return(
+                <tr key={part.id} id={part.id}>
+                    <td className="left">{part.prdRef}</td>
+                    <td>{part.nstThickness}</td>
+                    <td>{part.quantity}</td>
+                    <td></td>
+                </tr>)
+               }) :
+               filtered.map( part =>{
+                return(
+                <tr key={part.id} id={part.id}>
+                    <td className="left">{part.prdRef}</td>
+                    <td>{part.nstThickness}</td>
+                    <td>{part.quantity}</td>
+                    <td></td>
+                </tr>)
+               })}
+            </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
